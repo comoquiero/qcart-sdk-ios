7fdd39d345e0f5c6a4ff64deab04c48395c299ce
ec2-user <ec2-user@ip-172-31-19-213.ec2.internal>
2025-08-30 13:00:30 +0000

    merge

--- Tests/QCartTestAppUIKit/QCartTestAppUIKit/AppDelegate.swift
+++ Tests/QCartTestAppUIKit/QCartTestAppUIKit/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import QCartSDK
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -29,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // Custom URL scheme (for simulator testing)
-    func application(_ app: UIApplication,
+   func application(_ app: UIApplication,
                      open url: URL,
                      options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         handleQcartDeeplink(url: url)
