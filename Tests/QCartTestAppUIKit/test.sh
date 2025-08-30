54856db9c8bd66aa7c91934d143fcec859beb5d2
Qcart ComoQuiero <trollderiu@gmail.com>
2025-08-30 15:05:37 +0200

    separate test schemas

--- Tests/QCartTestAppUIKit/test.sh
+++ Tests/QCartTestAppUIKit/test.sh
@@ -2,4 +2,4 @@
 # xcrun simctl openurl booted "https://test.abc?qcart=true&skus=sku1:2,sku2:5"
 
 # SIMLATOR APP DEEPLINK:
-xcrun simctl openurl booted "qcarttest://test.abc?qcart=true&skus=sku1:2,sku2:5"
+xcrun simctl openurl booted "qcarttestuikit://test.abc?qcart=true&skus=sku1:2,sku2:5"
